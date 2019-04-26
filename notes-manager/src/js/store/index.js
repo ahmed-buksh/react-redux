efbb0b88787e2b1249e800ad63363d1323eff985
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2019-04-26 15:34:50 +0500

    Redux dev tool integrated

--- notes-manager/src/js/store/index.js
+++ notes-manager/src/js/store/index.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers/index';
+import { validateNote } from '../middleware/index';
 
-const store = createStore(rootReducer);
+const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    storeEnhancers(applyMiddleware(validateNote)),
+);
 
 export default store;

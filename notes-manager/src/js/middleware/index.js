2cd203558d7cc11ab0196e6b8887a25cc08f9408
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2019-04-26 15:32:33 +0500

    Middleware added and note validation is applied in middleware

---
+++ notes-manager/src/js/middleware/index.js
@@ -0,0 +1,14 @@
+import { ADD_NOTE } from "../constants/action-types";
+
+export function validateNote({ dispatch }) {
+    return function(next) {
+        return function(action) {
+            if (action.type === ADD_NOTE) {
+                if (action.payload.title.length === 0) {
+                    return dispatch({ type: "FOUND EMPTY NOTE" })
+                }
+            }
+            return next(action);
+        };
+    };
+}
